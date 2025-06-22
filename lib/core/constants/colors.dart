7906759e80690d9df3468eafe7f5e94736129a32
defarrel <Defarrel>
2025-06-22 21:11:35 +0700

    Menambahkan logo

--- lib/core/constants/colors.dart
+++ lib/core/constants/colors.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static const primary = Color(0xFF2FA66A); // hijau bibit
+  static const primary = Color(0xFF2FA66A); 
   static const background = Color(0xFFF6F6F6);
   static const textPrimary = Color(0xFF1E1E1E);
   static const grey = Color(0xFF9E9E9E);
