b7099d605986c38b46e672ed21cdd501093d67bb
defarrel <Defarrel>
2025-06-25 13:23:39 +0700

    Membuat auth response model saja dan menghapus login response

--- lib/data/models/response/auth/auth_response_mode.dart
+++ lib/data/models/response/auth/auth_response_mode.dart
@@ -1,65 +1,53 @@
 import 'dart:convert';
 
 class AuthResponseModel {
-    final String? message;
-    final int? statusCode;
-    final User? user;
+  final String? token;
+  final User? user;
 
-    AuthResponseModel({
-        this.message,
-        this.statusCode,
-        this.user,
-    });
+  AuthResponseModel({
+    this.token,
+    this.user,
+  });
 
-    factory AuthResponseModel.fromJson(String str) => AuthResponseModel.fromMap(json.decode(str));
+  factory AuthResponseModel.fromJson(String str) =>
+      AuthResponseModel.fromMap(json.decode(str));
 
-    String toJson() => json.encode(toMap());
+  factory AuthResponseModel.fromMap(Map<String, dynamic> json) =>
+      AuthResponseModel(
+        token: json["token"],
+        user: json["user"] == null ? null : User.fromMap(json["user"]),
+      );
 
-    factory AuthResponseModel.fromMap(Map<String, dynamic> json) => AuthResponseModel(
-        message: json["message"],
-        statusCode: json["status_code"],
-        user: json["data"] == null ? null : User.fromMap(json["data"]),
-    );
-
-    Map<String, dynamic> toMap() => {
-        "message": message,
-        "status_code": statusCode,
-        "data": user?.toMap(),
-    };
+  Map<String, dynamic> toMap() => {
+        "token": token,
+        "user": user?.toMap(),
+      };
 }
 
 class User {
-    final int? id;
-    final String? name;
-    final String? email;
-    final String? role;
-    final String? token;
+  final int? id;
+  final String? name;
+  final String? email;
+  final String? role;
 
-    User({
-        this.id,
-        this.name,
-        this.email,
-        this.role,
-        this.token,
-    });
+  User({
+    this.id,
+    this.name,
+    this.email,
+    this.role,
+  });
 
-    factory User.fromJson(String str) => User.fromMap(json.decode(str));
-
-    String toJson() => json.encode(toMap());
-
-    factory User.fromMap(Map<String, dynamic> json) => User(
+  factory User.fromMap(Map<String, dynamic> json) => User(
         id: json["id"],
         name: json["name"],
         email: json["email"],
         role: json["role"],
-        token: json["token"],
-    );
+      );
 
-    Map<String, dynamic> toMap() => {
+  Map<String, dynamic> toMap() => {
         "id": id,
         "name": name,
         "email": email,
         "role": role,
-        "token": token,
-    };
+      };
 }
