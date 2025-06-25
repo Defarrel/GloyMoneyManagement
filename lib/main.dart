41e5ebe6360d8ad08002145c7167451606d0d1ec
defarrel <Defarrel>
2025-06-25 13:32:00 +0700

    Memanggil multiblocprovider di main dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
-import 'presentation/splash/pages/splash_screen.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:gloymoneymanagement/data/repository/auth_repository.dart';
+import 'package:gloymoneymanagement/presentation/auth/bloc/login/login_bloc.dart';
+import 'package:gloymoneymanagement/presentation/splash/pages/splash_screen.dart';
+import 'package:gloymoneymanagement/services/service_http_client.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,13 +14,22 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'GMM App',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.green,
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => LoginBloc(
+            authRepository: AuthRepository(ServiceHttpClient()),
+          ),
+        ),
+      ],
+      child: MaterialApp(
+        title: 'GMM App',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primarySwatch: Colors.green,
+        ),
+        home: const SplashScreen(),
       ),
-      home: const SplashScreen(),
     );
   }
 }
